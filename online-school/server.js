d5b823c814ad7c969c21a779ff9b7d25c1feeedb
indrajit_almabase <indrajit@almabase.com>
2020-09-02 23:47:49 +0530

    Base files

---
+++ online-school/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://schools:GC00eB1WtiM34a6D@schools.l9qgr.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
