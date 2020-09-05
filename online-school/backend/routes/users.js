29f03e9af14be1d84b36e997e086d7345eed018e
indrajit_almabase <indrajit@almabase.com>
2020-09-05 17:11:37 +0530

    Added patch

--- online-school/backend/routes/users.js
+++ online-school/backend/routes/users.js
@@ -7,20 +7,38 @@ router.route('/').get((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/getbyemail').get((req, res) => {
+  const user = User.find().where("email" , req.body.email);
+  console.log(user);
+});
+
+router.route('/').patch(async (req, res) => {
+  const filter = { email : req.body.email };
+  const update = req.body;
+  let doc = await User.findOneAndUpdate(filter, update, {
+  new: true,
+  });
+  if(doc==null)
+    res.status(400).json('Error');
+  else
+    res.status(200).json(doc);
+});
+
+
 router.route('/add').post((req, res) => {
-  const username = req.body.username;
+  const password = req.body.password;
   const userType = req.body.userType;
   const name = req.body.name;
   const email = req.body.email;
   const phone = req.body.phone;
   const school = req.body.school;
-  const roll = req.body.roll;
+  const roll = req.body.roll?req.body.roll:null;
 
   const newUser = new User({
-    username,
+    email,
+    password,
     userType,
     name,
-    email,
     phone,
     school,
     roll,
@@ -30,5 +48,7 @@ router.route('/add').post((req, res) => {
     .then(() => res.json('User added!'))
     .catch(err => res.status(400).json('Error: ' + err));
 });
+
+router.route(`/`)
 
 module.exports = router;
