c65de614676077abe65dc74c1aec28e9da3a2881
indrajit_almabase <indrajit@almabase.com>
2020-10-10 20:34:20 +0530

    Files managed

--- online-school/src/components/dashboard.js
+++ online-school/src/components/dashboard.js
@@ -1,55 +1,17 @@
-import React, { Component, useState, useEffect } from "react";
-import { Cookies } from "react-cookie";
-import Cookies1 from "js-cookie";
+import React, { Component} from "react";
+
+import StudentDashboard from './dashboard/studentDashboard'
+import TeacherDashboard from './dashboard/teacherDashboard'
 
 export default class Dashboard extends Component {
   constructor() {
     super();
-    this.state = {
-      classrooms: [],
-    };
-  }
-  async componentDidMount() {
-    await fetch("/classrooms")
-      .then((response) => response.json())
-      .then((Classrooms) => {
-        this.setState({ classrooms: Classrooms });
-      });
-  }
-  classRoomList() {
-    const classrooms = this.state.classrooms.map((classroom) => {
-      return (
-        <div>
-          <a href={`/classroom/${classroom._id}`}>{classroom.subjectName}</a>
-        </div>
-      );
-    });
-    return classrooms;
   }
   render() {
+    const {email,type} = this.props;
     return (
       <div>
-        <h3>Welcome to our website</h3>
-        <button
-          onClick={() => {
-            const cookies = new Cookies();
-            cookies.remove("token", { path: "/", domain: "localhost" });
-            this.props.history.push("/");
-          }}
-        >
-          Logout
-        </button>
-
-        <button
-          onClick={(e) => {
-            console.log(Cookies1.get("token"));
-            window.location = "/create-classroom";
-          }}
-        >
-          Create classroom
-        </button>
-
-        <div>{this.classRoomList()}</div>
+      {type===1?<TeacherDashboard {...this.props} />:<StudentDashboard {...this.props} />}
       </div>
     );
   }
---
+++ online-school/src/components/dashboard/studentDashboard.js
@@ -0,0 +1,48 @@
+import React, { Component, useState, useEffect } from "react";
+import { Cookies } from "react-cookie";
+import Cookies1 from "js-cookie";
+
+export default class StudentDashboard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      classrooms: [],
+    };
+  }
+  async componentDidMount() {
+    await fetch("/classrooms")
+      .then((response) => response.json())
+      .then((Classrooms) => {
+        this.setState({ classrooms: Classrooms });
+      });
+  }
+  classRoomList() {
+    const classrooms = this.state.classrooms.map((classroom) => {
+      return (
+        <div>
+          <a href={`/classroom/${classroom._id}`}>{classroom.subjectName}</a>
+        </div>
+      );
+    });
+    return classrooms;
+  }
+  render() {
+    const {email,type} = this.props;
+    return (
+      <div>
+        <h3>Welcome to our website</h3>
+        <button
+          onClick={() => {
+            const cookies = new Cookies();
+            cookies.remove("token", { path: "/", domain: "localhost" });
+            this.props.history.push("/");
+          }}
+        >
+          Logout
+        </button>
+
+        <div>{this.classRoomList()}</div>
+      </div>
+    );
+  }
+}
